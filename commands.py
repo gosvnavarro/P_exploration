4bb77a16de308cfddd68ddbc3f1811db9414b3d0
_gosvn <gabi.onavarro@gmail.com>
2022-05-02 14:43:46 -0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -3,3 +3,10 @@ a_dict = {'antigo_1': 'novo_1', 'antigo_2': 'novo_2'}
 
 for key, value in a_dict.items():
     df= df.withColumnRenamed(value, key)
+
+# Copiar tabela
+# modo_1
+df_aux = tabela_original
+
+# modo_2
+copy_in_stats = in_stats.select('*')
